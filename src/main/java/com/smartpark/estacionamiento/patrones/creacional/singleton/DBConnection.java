b2ac10ff7dc7e34ba1d4a97dd71bbf60e9188516
JesusL215 <jesus.jli215@gmail.com>
2025-11-14 19:33:11 -0500

    corrigiendo error de logica

--- src/main/java/com/smartpark/estacionamiento/patrones/creacional/singleton/DBConnection.java
+++ src/main/java/com/smartpark/estacionamiento/patrones/creacional/singleton/DBConnection.java
@@ -11,7 +11,7 @@ public class DBConnection {
     // !!! ACTUALIZA TU CONTRASEÑA AQUÍ !!!
     private static final String URL = "jdbc:mysql://localhost:3306/gestion_estacionamiento_db";
     private static final String USER = "root";
-    private static final String PASSWORD = "root215"; //
+    private static final String PASSWORD = "123"; //
 
     private DBConnection() {
         try {
